187d5dd11801052d6ca46a0e118e0cfae31179a6
Manlate823 <59461267+Manlate823@users.noreply.github.com>
2026-09-09 14:08:37 -0400

    Delete assets directory

--- assets/config.js
+++
@@ -1,8 +0,0 @@
-/* =========================================================
-   CONFIGURACIÓN DE MANLATE
-   Pega entre las comillas el enlace de descarga del programa.
-   Puede ser un enlace de MediaFire, Google Drive, GitHub u otro.
-   ========================================================= */
-window.MANLATE_CONFIG = {
-  downloadUrl: "https://download1640.mediafire.com/5nsszpdyvjfgtPzfdH17pZwdSz4ZTONa9GV73_VZzlj0hZi54Gn6eERw55U7bp4xWg9wXj1kSsh60niIL4QhQmkQSQbhz5tSHVrUxiNz101ScRSF-qp65UNx9XZvFBY3FYGeCRHQtuG7PfjthWYcvgpcl3Jk2PGnt37sIVJvM0UL1g/l7fjltbs3ys6b3k/Manlate-Facturacion-Setup-4.8.8.exe"
-};
